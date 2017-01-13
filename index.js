bd2062d22413b97e93d9208f8d096e23bce7916f
Casey Webb <notcaseywebb@gmail.com>
2017-01-12 19:12:35 -0600

    Enforce indentation

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ module.exports = {
     'array-bracket-spacing': [2, 'never'],
     'arrow-parens': [2, 'always'],
     'arrow-spacing': 2,
+    'brace-style': 2,
     'comma-dangle': 0,
+    'curly': 2,
+    'indent': [2, 2],
     'key-spacing': [2, {
       beforeColon: false,
       afterColon: true,
@@ -28,6 +31,7 @@ module.exports = {
     'no-console': 1,
     'no-const-assign': 2,
     'no-debugger': 1,
+    'no-multi-spaces': 2,
     'no-unused-vars': 1,
     'no-var': 2,
     'object-curly-spacing': [2, 'always'],
