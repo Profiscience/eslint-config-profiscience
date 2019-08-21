82a60112b18a0aa236241e8bcba29ee039a94187
Casey Webb <notcaseywebb@gmail.com>
2019-08-21 12:43:21 -0500

    feat: enable all recommended rulesets

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 
 module.exports = {
   extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
     'prettier/@typescript-eslint'
   ],
