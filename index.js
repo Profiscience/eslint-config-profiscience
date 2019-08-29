fabf278fcf22a08ab4d1a89d4c6dbb25e1df9c6d
Casey Webb <notcaseywebb@gmail.com>
2019-08-29 13:59:56 -0500

    feat: relax rules for test files

--- index.js
+++ index.js
@@ -22,7 +22,27 @@ module.exports = {
 
   rules: {
     'no-console': ['error', { allow: ['error', 'info', 'warn'] }],
-    '@typescript-eslint/explicit-function-return-type': ['warn', { allowExpressions: true }],
+    '@typescript-eslint/explicit-function-return-type': [
+      'warn',
+      { allowExpressions: true }
+    ],
     '@typescript-eslint/no-use-before-define': ['error', { functions: false }]
-  }
+  },
+
+  overrides: [
+    {
+      files: [
+        '**/__test__/**/*',
+        '**/__tests__/**/*',
+        '**/test/**/*',
+        '**/tests/**/*',
+        '**/test.*'
+      ],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-explicit-any': 'off'
+      }
+    }
+  ]
 }
